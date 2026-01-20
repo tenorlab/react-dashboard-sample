f6f66308fe74a548bf6eb585147a8994a33256b9
Damiano Fusco <me@damianofusco.com>
2026-01-20 17:56:39 -0500

    Simplified WidgetTotalOrders component props handling
    Removed WidgetKey constant and spread props into DashboardWidgetBase.

--- bundled-widgets/widget-total-orders/WidgetTotalOrders.tsx
+++ bundled-widgets/widget-total-orders/WidgetTotalOrders.tsx
@@ -2,23 +2,13 @@
 import {
   IDashboardWidget,
   IDashboardWidgetProps,
-  TDashboardWidgetKey,
 } from '@tenorlab/react-dashboard'
 import { DashboardWidgetBase, WrapperColumnContent } from '@tenorlab/react-dashboard'
-
-const WidgetKey: TDashboardWidgetKey = 'WidgetTotalOrders'
 
 export function WidgetTotalOrders(props: IDashboardWidgetProps): IDashboardWidget {
   return (
     <DashboardWidgetBase
-      widgetKey={WidgetKey}
-      title="Total Orders"
-      parentWidgetKey={props.parentWidgetKey}
-      index={props.index}
-      maxIndex={props.maxIndex}
-      isEditing={props.isEditing}
-      onRemoveClick={props.onRemoveClick}
-      onMoveClick={props.onMoveClick}
+      {...props}
     >
       <WrapperColumnContent>
         <div className="dashboard-number number-xl text-primary">1,250</div>
